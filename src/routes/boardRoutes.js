3b95bb57d1156c8727bc0de9ac9f12fd31d7a65e
calebkeener13 <calebkeener13@gmail.com>
2025-10-22 16:19:49 -0400

    updated routing paths for boards

--- src/routes/boardRoutes.js
+++ src/routes/boardRoutes.js
@@ -2,10 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 // import board controller
-const { getAllBoards, getBoardById } = require('../controllers/boards/boardController')
+const { createNewBoard, getBoardById, getAllBoards } = require('../controllers/boards/boardController')
 
+// GET routes
 router.get('/all/', getAllBoards)
-router.get('/:id', getBoardById)
+router.get('/:id/', getBoardById)
+
+// POST routes
+router.post('/', createNewBoard);
+
+// PUT routes
 
 
 module.exports = router
