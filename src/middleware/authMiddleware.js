30a349e59b74298695ae4ad09049f1ee6d6ad036
calebkeener13 <calebkeener13@gmail.com>
2025-10-26 20:11:53 -0400

    fixed bugs in the verifyToken hanlder

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,24 +1,29 @@
 const jwt = require('jsonwebtoken')
 
-const verifyToken = (req, res) => {
+const verifyToken = (req, res, next) => {
     const authHeader = req.headers["authorization"];
+    if (!authHeader || !authHeader.startsWth("Bearer ")) {
+        return res.status(403).json({
+            Error: "Access Denied: No Valid Token Provided"
+        });
+    }
+
     const incomingToken = authHeader.split(' ')[1]
 
     if (!incomingToken) {
         return res.status(403).json({
-            message: "Access Denied"
+            Error: "Access Denied"
         });
     }
 
-    const decodedPayload = jwt.verify(incomingToken, process.env.JWT_SECRET);
-
-    if (!decodedPayload) {
-        return res.status(403).json({
-            Error: "Invalid or Expired Token"
-        });
+    try {
+        const decodedPayload = jwt.verify(incomingToken, process.env.JWT_SECRET);
+        req.user = decodedPayload;
+        next();
+    } catch (error) {
+        console.error(error);
+        return res.status(403).json({ error: "Invalid or expired token" });
     }
-
-    req.user = decodedPayload;
 };
 
 module.exports = verifyToken;
